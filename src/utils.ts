8f0f36e6691b725ad51f0979955efd1bdadbd797
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2024-05-18 01:07:59 +0200

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,7 @@ export type Decorator<D extends Function> = (d?: D) => D
 export type MouseEventHandlerDecorator = Decorator<EventHandler<MouseEvent<HTMLButtonElement>>>
 
 const seen: unknown[] = [];
-export const cloneObject = <T>(o: T): T => JSON.parse(JSON.stringify(o, function(key, val) {
+export const cloneObject = <T>(o: T): T => JSON.parse(JSON.stringify(o, function(_, val) {
   if (val != null && typeof val == "object") {
     if (seen.indexOf(val) >= 0) {
       return;
